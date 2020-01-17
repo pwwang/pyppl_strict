24df57d16fe4c787ebee8f89499880aac3c3d9ce
pwwang <pwwang@pwwang.com>
2020-01-17 11:59:13 -0600

    0.0.5

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ readme = ''
 setup(
     long_description=readme,
     name='pyppl_strict',
-    version='0.0.4',
+    version='0.0.5',
     description='More strict check of job success for PyPPL',
     python_requires='==3.*,>=3.6.0',
     author='pwwang',
